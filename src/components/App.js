e93d27527e0826792f178a2421a2a44627fc0d62
BohdanHromchak <gromchakbogdan@gmail.com>
2023-02-25 00:35:18 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,62 @@
-// import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid'
 const { Component } = require("react");
-
-
-
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [{id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},],
+    filter: '',
     name: '',
     number: ''
   }
 
-  handleChange = (event) => {
-const {name, value} = event.currentTarget
+handleChange = (event) => {
+      const {name, value} = event.currentTarget
+      this.setState({[name]: value})
+    }
 
-this.setState({[name]: value})
+handleSubmit = (event) => {
+      event.preventDefault()
+      this.setState({name: "", number: ""})
+
+      const newContact = {
+        id: nanoid(),
+        name: this.state.name,
+        number: this.state.number
+      }
+   
+    this.setState((prevState)=> {
+     if(prevState.contacts.find(contact => contact.name.includes(this.state.name))) {
+      return alert(`${this.state.name} is already in contacts.`)
+     }
+      
+    return {
+    contacts: [...prevState.contacts, newContact]
+    }
+    })
   }
+
+handleSearch = () => {
+    const {contacts, filter} = this.state
+   return contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()))
+
+  }
+
+handleDelete = (id) => {
+  this.setState((prevState) => {
+    return {
+      contacts: prevState.contacts.filter((contact) => contact.id !== id)
+    }
+  })
+}
 
   render() {
     return(
       <>
-<form>
+      <h1>Phonebook</h1>
+<form onSubmit={this.handleSubmit}>
 
   <label>Name
       <input
@@ -49,9 +85,24 @@ this.setState({[name]: value})
 <button>Add contact</button>
 
 </form>
+
+<label>Find contacts by name
+<input
+  type="text"
+  name="filter"
+  value={this.state.filter}
+  onChange={this.handleChange}
+/>
+</label>
+
 <h2>Contacts</h2>
 
-<ul></ul>
+<ul>
+{this.handleSearch().map(({id, name, number}) => (
+    <li key={id}>{name}: {number}<button onClick={() => this.handleDelete(id)}>Delete</button></li>
+  ))}
+
+</ul>
       </>
     )
   }
