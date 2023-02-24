4efd25a5ca93d109b3847d629829be9f386032e5
BohdanHromchak <gromchakbogdan@gmail.com>
2023-02-24 19:57:20 +0200

    upd

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import { nanoid } from 'nanoid'
+const { Component } = require("react");
+
+
+
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  }
+
+  handleChange = (event) => {
+const {name, value} = event.currentTarget
+
+this.setState({[name]: value})
+  }
+
+  render() {
+    return(
+      <>
+<form>
+
+  <label>Name
+      <input
+  type="text"
+  name="name"
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+  value={this.state.name}
+  onChange={this.handleChange}
+/>
+</label>
+
+<label>Number
+      <input
+  type="tel"
+  name="number"
+  pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+  title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+  required
+  value={this.state.number}
+  onChange={this.handleChange}
+/>
+</label>
+
+<button>Add contact</button>
+
+</form>
+<h2>Contacts</h2>
+
+<ul></ul>
+      </>
+    )
+  }
+}
+export default App
